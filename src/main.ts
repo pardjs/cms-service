22a43ad76bf8e11a9b4a48588797b7ea6246b3ea
BaiGuodong <bgd_sh@hotmail.com>
2019-04-16 21:42:17 +0800

    chore(): init project with @pardjs/starter

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { config } from 'dotenv';
+config();
+
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+import { PORT } from './constants';
+import {
+  logger,
+  corsOptions,
+  ValidationPipe,
+  HttpExceptionFilter,
+} from '@pardjs/common';
+// tslint:disable-next-line:no-var-requires
+const { version } = require('../package.json');
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  const options = new DocumentBuilder()
+    .setTitle('Pardjs CMS Service')
+    .setDescription('The Pardjs CMS Service API description')
+    .setVersion(version)
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  const apiDocPath = 'api-doc';
+  SwaggerModule.setup(apiDocPath, app, document);
+  await app.listen(PORT);
+  logger.info(`service started at ${PORT}`);
+  logger.info(`find api doc at http://0.0.0.0:${PORT}/${apiDocPath}`);
+}
+bootstrap();
